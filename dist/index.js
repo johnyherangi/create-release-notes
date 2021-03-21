6b7aed6a67d641d2da9039423f22770ea4b54545
johnyherangi <johnyherangi@gmail.com>
2021-03-21 17:16:11 +1300

    updates

--- dist/index.js
+++ dist/index.js
@@ -7,67 +7,99 @@ require('./sourcemap-register.js');module.exports =
 
 "use strict";
 
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__webpack_require__(186));
+const core = __importStar(__webpack_require__(186));
 const github_1 = __webpack_require__(438);
-async function main() {
-    if (process.env.GITHUB_TOKEN === undefined) {
-        core_1.default.setFailed('env.GITHUB_TOKEN is not set');
-        return;
-    }
-    let baseRef = '';
-    const headRef = core_1.default.getInput('head-ref');
-    const format = core_1.default.getInput('format');
-    const github = github_1.getOctokit(process.env.GITHUB_TOKEN);
-    const { owner, repo } = github_1.context.repo;
-    return github.repos
-        .getLatestRelease({ owner, repo })
-        .then((release) => github.request('GET /repos/:owner/:repo/compare/:baseRef...:headRef', {
-        owner,
-        repo,
-        baseRef: (baseRef = release.data.tag_name),
-        headRef,
-    }), () => github
-        .request('GET /repos/:owner/:repo/commits/:headRef', {
-        owner,
-        repo,
-        headRef,
-    })
-        .then((response) => ({
-        data: {
-            commits: [response.data],
-        },
-    })))
-        .then((response) => response.data.commits
-        .map((commit) => ({
-        author: commit.author.login,
-        subject: commit.commit.message.split('\n')[0],
-        message: commit.commit.message,
-    }))
-        .reverse())
-        .then((commits) => {
-        if (commits.length === 0) {
-            core_1.default.setFailed(`No commits found between refs ${baseRef}...${headRef}`);
-            return;
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            if (process.env.GITHUB_TOKEN === undefined) {
+                core.setFailed('env.GITHUB_TOKEN is not set');
+                return;
+            }
+            let baseRef = '';
+            const headRef = core.getInput('head-ref');
+            const format = core.getInput('format');
+            const github = github_1.getOctokit(process.env.GITHUB_TOKEN);
+            const { owner, repo } = github_1.context.repo;
+            return github.repos
+                .getLatestRelease({ owner, repo })
+                .then((release) => github.request('GET /repos/:owner/:repo/compare/:baseRef...:headRef', {
+                owner,
+                repo,
+                baseRef: (baseRef = release.data.tag_name),
+                headRef,
+            }), () => github
+                .request('GET /repos/:owner/:repo/commits/:headRef', {
+                owner,
+                repo,
+                headRef,
+            })
+                .then((response) => ({
+                data: {
+                    commits: [response.data],
+                },
+            })))
+                .then((response) => response.data.commits
+                .map((commit) => ({
+                author: commit.author.login,
+                subject: commit.commit.message.split('\n')[0],
+                message: commit.commit.message,
+            }))
+                .reverse())
+                .then((commits) => {
+                if (commits.length === 0) {
+                    core.setFailed(`No commits found between refs ${baseRef}...${headRef}`);
+                    return;
+                }
+                core.setOutput('release_name', commits[0].subject);
+                let releaseNotes = '';
+                for (const commit of commits) {
+                    releaseNotes += format
+                        .replace('{{author}}', commit.author)
+                        .replace('{{subject}}', commit.subject)
+                        .replace('{{message}}', commit.message);
+                    releaseNotes += '%0A';
+                }
+                core.setOutput('release_notes', releaseNotes);
+            })
+                .catch((error) => core.setFailed(error.message));
         }
-        core_1.default.setOutput('release_name', commits[0].subject);
-        let releaseNotes = '';
-        for (const commit of commits) {
-            releaseNotes += format
-                .replace('{{author}}', commit.author)
-                .replace('{{subject}}', commit.subject)
-                .replace('{{message}}', commit.message);
-            releaseNotes += '%0A';
+        catch (error) {
+            core.setFailed(error.message);
         }
-        core_1.default.setOutput('release_notes', releaseNotes);
-    })
-        .catch((error) => core_1.default.setFailed(error.message));
+    });
 }
 main();
-//# sourceMappingURL=main.js.map
+
 
 /***/ }),
 
