b994b789310f583a5e00fface0e1f9e96ec208b1
johnyherangi <johnyherangi@gmail.com>
2021-03-21 16:53:49 +1300

    update template

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,75 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import core from '@actions/core'
+import { context, getOctokit } from '@actions/github'
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+type Commit = {
+    author: { login: string }
+    commit: { message: string }
 }
 
-run()
+async function main() {
+    if (process.env.GITHUB_TOKEN === undefined) {
+        core.setFailed('env.GITHUB_TOKEN is not set')
+        return
+    }
+
+    let baseRef = ''
+    const headRef = core.getInput('head-ref')
+    const format = core.getInput('format')
+
+    const github = getOctokit(process.env.GITHUB_TOKEN)
+    const { owner, repo } = context.repo
+    return github.repos
+        .getLatestRelease({ owner, repo })
+        .then(
+            (release) =>
+                github.request('GET /repos/:owner/:repo/compare/:baseRef...:headRef', {
+                    owner,
+                    repo,
+                    baseRef: (baseRef = release.data.tag_name),
+                    headRef,
+                }),
+            () =>
+                github
+                    .request('GET /repos/:owner/:repo/commits/:headRef', {
+                        owner,
+                        repo,
+                        headRef,
+                    })
+                    .then((response) => ({
+                        data: {
+                            commits: [response.data as Commit],
+                        },
+                    })),
+        )
+        .then((response) =>
+            response.data.commits
+                .map((commit: Commit) => ({
+                    author: commit.author.login,
+                    subject: commit.commit.message.split('\n')[0],
+                    message: commit.commit.message,
+                }))
+                .reverse(),
+        )
+        .then((commits) => {
+            if (commits.length === 0) {
+                core.setFailed(`No commits found between refs ${baseRef}...${headRef}`)
+                return
+            }
+
+            core.setOutput('release_name', commits[0].subject)
+
+            let releaseNotes = ''
+            for (const commit of commits) {
+                releaseNotes += format
+                    .replace('{{author}}', commit.author)
+                    .replace('{{subject}}', commit.subject)
+                    .replace('{{message}}', commit.message)
+                releaseNotes += '%0A'
+            }
+
+            core.setOutput('release_notes', releaseNotes)
+        })
+        .catch((error) => core.setFailed(error.message))
+}
+
+main()
